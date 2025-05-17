4eb0c8fb2324e1d6cf7b755ac5ab11b9ed6bf7b0
Devansh Mohanty <devanshmohanty763@gmail.com>
2025-05-17 17:13:56 +0000

    Make the following changes:
    ,
    
    Find this element:
    
      <Header>
    
    with the text content:
    
        HomeAbout UsBoard MembersTournamentsCoachingRankingContact UsOpen menu
    
    Relevant files:
    
        - https://6000-firebase-studio-1747396635269.cluster-iktsryn7xnhpexlu6255bftka4.cloudworkstations.dev/_next/static/chunks/node_modules_next_dist_compiled_2ce9398a._.js
    
    And change it as follows:
    
    remove the rounded curves

--- src/components/layout/header.tsx
+++ src/components/layout/header.tsx
@@ -21,7 +21,7 @@ const Header = () => {
   }, []);
 
   return (
-    <header className="sticky top-0 z-50 w-full border-b border-sidebar-border/40 bg-sidebar text-sidebar-foreground shadow-md rounded-lg">
+    <header className="sticky top-0 z-50 w-full border-b border-sidebar-border/40 bg-sidebar text-sidebar-foreground shadow-md"> {/* Removed rounded-lg */}
       <div className="container mx-auto flex h-24 items-center justify-between px-4 sm:px-6 lg:px-8">
         <Link href="/" className="flex items-center gap-2" onClick={() => setIsSheetOpen(false)}>
           <Image 
